d9894bea0332d90f1c8f7a589b35c18205787462
Pola <146646425+poladudek@users.noreply.github.com>
2023-11-04 13:11:59 +0100

    Add files via upload

---
+++ 3zadanie.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main()
+
+{
+    int n;
+
+    printf("Podaj zakres gorny przedzialu w ktorym\
+    sprawdzane beda liczby pierwsze:\n");
+    scanf("%f", n);
+    if (n>2)
+    {
+        int
+    }
+
+}
